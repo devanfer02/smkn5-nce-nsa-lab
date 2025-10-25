653f332d3b464186b9f045809e65968fec478d07
devanfer <ferrelvsc20@gmail.com>
2025-10-25 12:55:58 +0700

    chore: create centralized playbook

--- shell/ans-playbook-all.sh
+++ shell/ans-playbook-all.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/imports/colors.sh"
 
 TOTAL_HOST=9
-PLAYBOOKS=($(ls ansible/playbooks))
 
 cd ansible || exit
 
@@ -14,17 +13,14 @@ count=$(ansible kelompok -m ping | grep -c "SUCCESS")
 while [ $count -ne $TOTAL_HOST ]; do 
     echo -e "${YELLOW}Only $count hosts are reachable!${RESET}"
     echo -e "${YELLOW}Waiting for all $TOTAL_HOST hosts to be reachable...${RESET}"
-
     sleep 2
     count=$(ansible kelompok -m ping | grep -c "SUCCESS")
 done 
 
 echo -e "${GREEN}All $TOTAL_HOST hosts are reachable!${RESET}"
 
-for pbook in "${PLAYBOOKS[@]}"; do 
-    printf '=%.0s' {1..65}; echo
-    echo -e "${BLUE}Running playbook: $pbook${RESET}"
-    ansible-playbook -e @inventory/vars.yaml "playbooks/$pbook"
-done 
+echo -e "${BLUE}Running all playbooks in ansible/playbooks directory${RESET}"
+
+ansible-playbook -e @inventory/vars.yaml "playbooks/provisioning.yaml"
 
 echo -e "${GREEN}All playbooks completed successfully!${RESET}"
